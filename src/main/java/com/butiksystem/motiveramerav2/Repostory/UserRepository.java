e738df39f9ea30eb12a448d2fdf706dc0f5bcbbc
Ahmad <Ahmad.abdulmutaleb@outlook.com>
2025-08-31 13:54:49 +0200

    Repository Created

---
+++ src/main/java/com/butiksystem/motiveramerav2/Repostory/UserRepository.java
@@ -0,0 +1,12 @@
+package com.butiksystem.motiveramerav2.Repostory;
+
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findById(int Id);
+
+    User findUserByEmail(String email);
+}
